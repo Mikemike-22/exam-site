abe273fc50d593f250ae07a2743f932cc491a7d9
Michael Korir <mikor22@gmail.com>
2019-02-25 11:17:36 +0300

    update ja file

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+    function showQuestions(questions, quizContainer){
+      //code
+    }
+    function showResults(question, quizContainer, resutsContainer){
+      //code
+    }
+    //show the questions
+    showQuestions(questions, quizContainer);
+
+    //when user clicks submit, show results
+    submitButton.onclick=function(){
+
+    }
+}
+
+var myQuestions =[
+  {
+    question: "Which is not programming language?"
+    answer:{
+      a: "HTML",
+      b: "Java Script",
+      c: "CSS",
+      d: "PHP"
+
+    }
+  }
+]
