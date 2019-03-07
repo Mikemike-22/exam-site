5a6153d62dc41a7349e7ee113208032a746fa114
Michael Korir <mikor22@gmail.com>
2019-03-07 09:18:23 +0300

    rectify indenting and spacing errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
-var testResults = function(Q1, Q2 ,Q3 ,Q4 ,Q5 ,Q6 ){
-return Q1+Q2+Q3+Q4+Q5+Q6;
+// var results =[Q1, Q2 ,Q3 ,Q4 ,Q5 ,Q6 ]
 
-
+function testResults(results){
+  var sum=0;
+  for (var i=0;i<results.length; i++){
+    sum+=results[i];
+  }
+  return sum;
 }
-   $(document).ready(function(){
-  $("form#test").submit(function(event){
 
-    var Q1 = parseInt($("input:radio[name=opt]:checked").val());
+   $(document).ready(function(){
+  $("#submitButton").click(function(event){
+
+
+    var Q1 = $("input:radio[name=opt]:checked").val();
     var Q2 = parseInt($("input:radio[name=assgt]:checked").val());
     var Q3 = parseInt($("input:radio[name=java]:checked").val());
     var Q4=
@@ -16,9 +22,14 @@ return Q1+Q2+Q3+Q4+Q5+Q6;
     var Q6=
     parseInt($("input:radio[name=user]:checked").val());
 
-    var result = testResults(Q1, Q2, Q3, Q4, Q5, Q6);
+    // var result = testResults(Q1, Q2, Q3, Q4, Q5, Q6);
+    // document.getElementById("score").innerHTML=result;
 
-    $("#score").text("your score is:"+ result);
+    var results = Q1+Q2+Q3+Q4+Q5+Q6;
+
+    var sum = testResults(results);
+    $("score").text(sum);
+    // $("#score").text( result);
       event.preventDefault();
 
 
